8df025a0a0660372c0ca3167be8e79de4c41a24c
KarChikey <kartikeynegi2002@gmail.com>
2025-10-17 23:04:11 +0530

    add

--- Extract_api.py
+++ Extract_api.py
@@ -4,6 +4,7 @@ import json
 import os
 from dotenv import load_dotenv
 from urllib3 import response
+import pandas as pd
 
 load_dotenv()
 
@@ -52,3 +53,28 @@ def Extract_Api(city_name,API_KEY):
 # with open("weather_data.json", "w") as json_file:
 #     json.dump(weather_data, json_file, indent=2)
 
+def transform(all_data):
+    data=[]
+    for city ,city_data in all_data.items():
+        if city is not None:
+            for forecast in city_data['forecast']:
+                data.append({
+                    'city':city_data['city_name'],
+                    'country':city_data['country'],
+                    'datetime': forecast['datetime'],
+                    'temperature': forecast['temperature'],
+                    'humidity': forecast['humidity'],
+                    'weather': forecast['weather'],
+                    'description': forecast['description'],
+                    'wind_speed': forecast['wind_speed']
+                })
+    df=pd.DataFrame(data)
+    return df
+
+print("Extrating data from the API")
+weather_data=Extract_Api(city_name,API_KEY)
+
+print("Transforming the data")
+df=transform(weather_data)
+
+df.to_csv("weather_data.csv",index=False)
