001b3bb7cfb72de15e12eb37304d905fd52e00f1
KarChikey <kartikeynegi2002@gmail.com>
2025-10-18 21:50:03 +0530

    add

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Extract_api import Extract_Api
 from transform1 import transform,transform_raw
+from load import load_data_to_s3
+from datetime import datetime
 import os
 
 API_KEY=os.getenv("Weather_Api")
+Bucket_Name=os.getenv("AWS_BUCKET")
 
 def main():
    city_name=["Delhi", "Mumbai", "Dehradun", "Lucknow", "Chandigarh"]
@@ -16,16 +19,20 @@ def main():
    print("transforming data")
    df1=transform(df)
    
+   current_date = datetime.now().strftime("%Y-%m-%d")
    folder_path = "data"
-   if os.path.exists(folder_path):
-        os.system(f'rmdir /S /Q {folder_path}')  
-   os.makedirs(folder_path)                      
+   os.makedirs(folder_path, exist_ok=True)
+    
+   file_name = f"{current_date}_weather_data.csv"
+   file_path = os.path.join(folder_path, file_name)
 
-   df1.to_csv(os.path.join(folder_path, "transformed_weather.csv"), index=False)
-   print(f"File saved at {folder_path}/transformed_weather.csv")
-   
-   
-   df1.to_csv("data/transformed_weather.csv",index=False)
-   
+   df1.to_csv(file_path, index=False)
+   print(f"File saved locally at: {file_path}")
+
+   print("Uploading to S3...")
+   load_data_to_s3(file_path, Bucket_Name)
+
+   print("ETL pipeline completed successfully!")
+
 if __name__=="__main__":
     main()
