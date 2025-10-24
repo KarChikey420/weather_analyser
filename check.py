d37144fc91c388c47dba866f8550b374b4f7db99
KarChikey <kartikeynegi2002@gmail.com>
2025-10-24 16:03:17 +0530

    add

--- check.py
+++ check.py
@@ -1,21 +1,14 @@
 import psycopg2
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-conn = psycopg2.connect(
-    host=os.getenv("REDSHIFT_HOST").split(":")[0],
-    dbname=os.getenv("REDSHIFT_DB"),
-    user=os.getenv("REDSHIFT_USER"),
-    password=os.getenv("REDSHIFT_PASSWORD"),
-    port=int(os.getenv("REDSHIFT_PORT",5439))
-)
-
-cur = conn.cursor()
-cur.execute("SELECT COUNT(*) FROM your_table_name;")
-count = cur.fetchone()[0]
-print(f"Total rows in table: {count}")
-
-cur.close()
-conn.close()
+try:
+    conn = psycopg2.connect(
+        host="kartik.220402651988.ap-south-1.redshift-serverless.amazonaws.com",
+        port=5432,
+        dbname="dev",
+        user="admin",
+        password="Morning0000",
+        sslmode='require'
+    )
+    print("✅ Connection successful!")
+except Exception as e:
+    print("❌ Connection failed:", e)
