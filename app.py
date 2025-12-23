8302375745d7e3d8b0f9deee9b2e8a7e6f9d0af4
KarChikey <kartikeynegi2002@gmail.com>
2025-12-23 19:09:46 +0530

    add

--- app.py
+++ app.py
@@ -1,67 +1,42 @@
-import streamlit as st
-import pandas as pd
-import psycopg2
-import plotly.express as px
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 from dotenv import load_dotenv
 import os
+from Extract_api import Extract_Api
+from transform1 import transform, transform_raw
+from load import upload_df_to_s3
+from s3_to_redsift import load_data_to_redshift
+
+app=FastAPI("Weather ETL API")
 
 load_dotenv()
 
-@st.cache_data
-def get_data():
-    """Fetch weather data from Redshift"""
-    con = psycopg2.connect(
-        host=os.getenv("REDSHIFT_HOST").split(":")[0],
-        dbname=os.getenv("REDSHIFT_DB"),
-        user=os.getenv("REDSHIFT_USER"),
-        password=os.getenv("REDSHIFT_PASSWORD"),
-        port=int(os.getenv("REDSHIFT_PORT", 5439))
-    )
-    query = "SELECT city_name, temperature, humidity_percent, date FROM weather_data LIMIT 1000;"
-    cur = con.cursor()
-    cur.execute(query)
-    colnames = [desc[0] for desc in cur.description]
-    rows = cur.fetchall()
-    df = pd.DataFrame(rows, columns=colnames)
-    con.close()
-    return df
-
-df = get_data()
-
-st.title("🌦 Weather Data Dashboard")
-
-st.sidebar.header("🔍 Filters")
-available_dates = sorted(df['date'].unique())
-selected_date = st.sidebar.selectbox("Select Date", available_dates)
-
-filtered_df = df[df['date'] == selected_date]
-
-cities = st.sidebar.multiselect("Select Cities", filtered_df['city_name'].unique())
-if cities:
-    filtered_df = filtered_df[filtered_df['city_name'].isin(cities)]
-
-st.markdown(f"### 📅 Weather Summary for **{selected_date}**")
-
-col1, col2 = st.columns(2)
-col1.metric("Average Temperature (°C)", round(filtered_df['temperature'].mean(), 2))
-col2.metric("Average Humidity (%)", round(filtered_df['humidity_percent'].mean(), 2))
-
-st.markdown("---")
-
-if not filtered_df.empty:
-    col1, col2 = st.columns(2)
-
-    with col1:
-        fig1 = px.bar(filtered_df, x='city_name', y='temperature',
-                      title="Temperature by City", color='city_name')
-        st.plotly_chart(fig1, use_container_width=True)
-
-    with col2:
-        fig2 = px.bar(filtered_df, x='city_name', y='humidity_percent',
-                      title="Humidity by City", color='city_name')
-        st.plotly_chart(fig2, use_container_width=True)
-else:
-    st.warning("No data available for this selection.")
-
-st.markdown("### 🧾 Data Preview")
- 
+class WheatherRequest(BaseModel):
+    city_names: list[str]
+    table_name:str
+    
+@app.post("/etl")
+def etl_process(request: WheatherRequest):
+    try:
+        API_KEY = os.getenv("Weather_Api")
+        Bucket_Name = os.getenv("AWS_BUCKET")
+        REDSHIFT_HOST = os.getenv("REDSHIFT_HOST").split(":")[0]
+        REDSHIFT_DB = os.getenv("REDSHIFT_DB")
+        REDSHIFT_USER = os.getenv("REDSHIFT_USER")
+        REDSHIFT_PASSWORD = os.getenv("REDSHIFT_PASSWORD")
+        REDSHIFT_PORT = int(os.getenv("REDSHIFT_PORT", 5439))
+        IAM_ROLE_ARN = os.getenv("IAM_ROLE_ARN")
+        TABLE_NAME = os.getenv("TABLE_NAME")
+        REGION = os.getenv("AWS_REGION")
+        
+        if not all([API_KEY, Bucket_Name, REDSHIFT_HOST, REDSHIFT_DB, REDSHIFT_USER, REDSHIFT_PASSWORD, IAM_ROLE_ARN, TABLE_NAME, REGION]):
+            return {"status": "error", "message": "One or more environment variables are missing."}
+        
+        raw_data= Extract_Api(request.city_names, API_KEY)
+        df_raw = transform_raw(raw_data)
+        df_clean = transform(df_raw)
+    
+    except Exception as e:
+        return {"status": "error", "message": f"Environment variable error: {e}"}
+        
